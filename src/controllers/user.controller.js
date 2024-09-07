ab3feb7338851b5bda0d55ead52162a3f1c201f3
Sumit Singh <ss1047r@gmail.com>
2024-09-07 23:10:22 +0530

    add register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,73 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  await res.status(200).json({
-    message: "Ok",
+  const { username, email, fullName, password } = req.body;
+  console.log("email :", email, "password :", password);
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  if (!emailRegex.test(email)) {
+    throw new ApiError(405, "Invalid email format");
+  }
+
+  const userExist = await User.findone({
+    $or: [{ username }, { email }],
   });
+
+  if (userExist) {
+    if (userExist.username === "username") {
+      throw new ApiError(409, "Username already exists");
+    }
+    if (userExist.email === "email") {
+      throw new ApiError(409, "Email already exists");
+    }
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError("Avatar is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await user.findById(user._id).select(
+    "-password -refreshToken"
+  )
+
+  if (!createdUser) {
+    throw new ApiError(500, "Internal Server Error")
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully")
+  )
+
 });
 
 export { registerUser };
