8d2fc09f4276287a38714455ada65da749c42325
Sumit Singh <ss1047r@gmail.com>
2024-09-07 19:07:23 +0530

    error resolved

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -6,17 +6,3 @@ const asyncHandler = (requestHandler) => {
 
 export { asyncHandler };
 
-// const asyncHandler = ()=>{}
-// const asyncHandler = (func) => () => {}
-// const asyncHandler = (func) => async() => {}    //higher order function
-
-// const asyncHandler = (fn) => async(req, res, next) => {
-//   try {
-//     await(req, res, next)
-//   } catch (error) {
-//     res.status(error.code || 500).json({
-//       success: false,
-//       message: error.message
-//     })
-//   }
-// }
